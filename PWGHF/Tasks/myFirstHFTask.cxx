32a04ece8c4046e400aef129bd04ee109f7d6bef
karlazertova <104975675+karlazertova@users.noreply.github.com>
2022-09-09 18:03:19 +0200

    collision selection

--- PWGHF/Tasks/myFirstHFTask.cxx
+++ PWGHF/Tasks/myFirstHFTask.cxx
@@ -1,19 +1,44 @@
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
+#include "Common/DataModel/EventSelection.h"
 
 using namespace o2;
 using namespace o2::framework;
 
-struct myFirstHFTask(){
-    HistogramRegistry registry{"registry"{
-            "eta", "pseudorapidity", {HistType::kTH1k, {{100, 0, 1}}}
+struct myFirstHFTask {
+    HistogramRegistry registry{"registry",{
+            {"etaBefore", "eta before selection", {HistType::kTH1F, {{100, 0, 1}}}},
+            {"etaAfter", "eta after selection", {HistType::kTH1F, {{100, 0, 1}}}}
         }
     };
-    void process(aod::Collision const&, aod::Tracks const& tracks){
-        registry.fill<aod::track::Eta>(HIST("eta"), tracks, aod::track::eta > 0.0f);
-        registry.fill<aod::track::Eta>(HIST("eta"), tracks, aod::track::eta < 1.0f);
-        for (auto Track : tracks){
-            registry.get<TH1>(HIST(eta))->Fill(track.eta());
+    void process(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, aod::Tracks const& tracks){
+
+	// fill histogram of collision vertex z
+
+        registry.get<TH1>(HIST("etaBefore"))->Fill(collision.posZ());	
+
+	// event selection
+
+        if (!collision.alias()[kINT7]) {
+        return;
+	}       // trigger selection
+
+	if (!collision.sel7()) {
+	return;
+	} // rejection of background (beam-gas events, pileup)
+
+	if (std::abs(collision.posZ()) > 10) {
+	return;
+	} // cut on collision vertex in z direction
+
+
+	// fill histogram of collision vertex z
+ 	registry.get<TH1>(HIST("etaAfter"))->Fill(collision.posZ());
+        
+	for (auto track : tracks){
+		// track selection
+            	
+		registry.get<TH1>(HIST("etaAfter"))->Fill(track.eta());
         }
     }
 };
